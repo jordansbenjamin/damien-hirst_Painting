b1b06c1c39cc58cf173e46e8bfeefbcd7e358a68
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-03 17:43:28 +1000

    Added initial movement to pen object with dots

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import colorgram
+from turtle import Turtle, Screen
+import random as rand
 
-colors = colorgram.extract('image.jpg', 12)
+# colors = colorgram.extract('image.jpg', 20)
 
 # print(colors[0].rgb)
 
@@ -29,6 +31,40 @@ colors = colorgram.extract('image.jpg', 12)
 #     rgb = color.rgb
 #     colors_list.append(tuple(rgb))
 
-colors_list = [tuple(color.rgb) for color in colors]
+# rgb_colors = [tuple(color.rgb) for color in colors]
 
-# print(colors_list)
+# print(rgb_colors)
+
+color_list = [(233, 239, 246), (246, 239, 242), (132, 166, 205), (221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), (39, 105, 157), (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157), (51, 111, 90), (35, 61, 55), (156, 33, 31), (17, 97, 71)]
+
+# 100 dots in total
+# 10x10
+# Dot size 20
+# Spaced by 50 paces
+
+pen = Turtle()
+pen.shape('classic')
+pen.pensize(20)
+
+screen = Screen()
+screen.colormode(255)
+
+def painting():
+    pen.penup()
+    pen.setposition(-230,-200)
+    directions = {
+        'east': 0,
+        'north': 90,
+        'west': 180,
+        'south': 270
+    }
+    
+    for _ in range(10):
+        pen.color(rand.choice(color_list))
+        pen.dot(20)
+        pen.penup()
+        pen.forward(50)
+
+painting()
+
+screen.exitonclick()
